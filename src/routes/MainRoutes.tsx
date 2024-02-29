6a1f0e27ab276caed9afb9768d8e3c076deaa731
lik3as <daviacosta2006@gmail.com>
2024-02-29 14:19:55 -0300

    initial commit

---
+++ src/routes/MainRoutes.tsx
@@ -0,0 +1,71 @@
+import { lazy } from 'react';
+
+// project-imports
+import MainLayout from 'layout/MainLayout';
+import CommonLayout from 'layout/CommonLayout';
+import Loadable from 'components/Loadable';
+import AuthGuard from 'utils/route-guard/AuthGuard';
+
+const MaintenanceError = Loadable(lazy(() => import('pages/maintenance/error/404')));
+const MaintenanceError500 = Loadable(lazy(() => import('pages/maintenance/error/500')));
+const MaintenanceUnderConstruction = Loadable(lazy(() => import('pages/maintenance/under-construction/under-construction')));
+const MaintenanceComingSoon = Loadable(lazy(() => import('pages/maintenance/coming-soon/coming-soon')));
+
+// render - main page
+const DashboardDefault = Loadable(lazy(() => import('pages/dashboard/default')));
+const DashboardAnalytics = Loadable(lazy(() => import('pages/dashboard/analytics')));
+
+// ==============================|| MAIN ROUTES ||============================== //
+
+const MainRoutes = {
+  path: '/',
+  children: [
+    {
+      path: '/',
+      element: (
+        <AuthGuard>
+          <MainLayout />
+        </AuthGuard>
+      ),
+      children: [
+        {
+          path: 'dashboard',
+          children: [
+            {
+              path: 'home',
+              element: <DashboardDefault />
+            },
+            {
+              path: 'analytics',
+              element: <DashboardAnalytics />
+            }
+          ]
+        }
+      ]
+    },
+    {
+      path: '/maintenance',
+      element: <CommonLayout />,
+      children: [
+        {
+          path: '404',
+          element: <MaintenanceError />
+        },
+        {
+          path: '500',
+          element: <MaintenanceError500 />
+        },
+        {
+          path: 'under-construction',
+          element: <MaintenanceUnderConstruction />
+        },
+        {
+          path: 'coming-soon',
+          element: <MaintenanceComingSoon />
+        }
+      ]
+    }
+  ]
+};
+
+export default MainRoutes;
